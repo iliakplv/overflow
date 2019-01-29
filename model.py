117ace6a24cad9821b52e503425d55bc01dfb2c4
Ilia Kopylov <ilia@springload.co.nz>
2019-01-30 11:59:12 +1300

    Implement input function (pandas)

--- model.py
+++ model.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
+import pandas as pd
 
 dataset_file = './data/so_survey_results_public.csv'
 
-feature_names = ['Hobby', 'OpenSource', 'Country']  # TODO
-record_defaults = [['No'], ['No']]  # TODO
+# TODO
+feature_names = ['Hobby', 'OpenSource', 'Country']
+record_defaults = [['No'], ['No']]
+target = ''
 
 
 def input_fn(file_path, perform_shuffle=False, repeat_count=1, batch_size=32):
     def decode_csv(line):
+        # TODO features and target
         parsed_line = tf.decode_csv(line, record_defaults)
         label = parsed_line[-1:]
         del parsed_line[-1]
@@ -25,3 +29,20 @@ def input_fn(file_path, perform_shuffle=False, repeat_count=1, batch_size=32):
     iterator = dataset.make_one_shot_iterator()
     batch_features, batch_labels = iterator.get_next()
     return batch_features, batch_labels
+
+
+def input_fn_pandas():
+    df = pd.read_csv(dataset_file)
+    x = df[feature_names]
+    y = df[target]
+
+    return tf.estimator.inputs.pandas_input_fn(
+        x,
+        y,
+        batch_size=32,
+        num_epochs=1,
+        shuffle=None,
+        queue_capacity=1000,
+        num_threads=4,
+        target_column=target
+    )

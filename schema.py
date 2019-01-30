f0a64bc2873abb89bc42f3ff49f10eb85f10447a
Ilia Kopylov <ilia@springload.co.nz>
2019-01-30 14:44:22 +1300

    Init feature defaults

--- schema.py
+++ schema.py
@@ -1,23 +1,25 @@
+na = 'N/A'
+
 target = 'CareerSatisfaction'
 
-# TODO
-feature_defaults = {'Hobby': '',
-                    'OpenSource': '',
-                    'Student': '',
-                    'Employment': '',
-                    'FormalEducation': '',
-                    'CompanySize': '',
-                    'YearsCoding': '',
-                    'YearsCodingProf': '',
-                    'JobSearchStatus': '',
-                    'LastNewJob': '',
-                    'WakeTime': '',
-                    'HoursComputer': '',
-                    'HoursOutside': '',
-                    'SkipMeals': '',
-                    'Exercise': '',
-                    'Age': '',
-                    'Dependents': ''}
+feature_defaults = {'Hobby': 'No',
+                    'OpenSource': 'No',
+                    'Student': 'No',
+                    'Employment': 'Employed full-time',
+                    'FormalEducation': 'Bachelor’s degree (BA, BS, B.Eng., etc.)',
+                    'UndergradMajor': na,
+                    'CompanySize': na,
+                    'YearsCoding': '3-5 years',
+                    'YearsCodingProf': '3-5 years',
+                    'JobSearchStatus': 'I’m not actively looking, but I am open to new opportunities',
+                    'LastNewJob': 'Less than a year ago',
+                    'WakeTime': na,
+                    'HoursComputer': na,
+                    'HoursOutside': na,
+                    'SkipMeals': na,
+                    'Exercise': na,
+                    'Age': na,
+                    'Dependents': na}
 
 feature_names = list(feature_defaults.keys())
 
